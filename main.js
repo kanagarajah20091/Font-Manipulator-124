abc44e282c041f4311e942417722c690aba68767
kanagarajah20091 <95323647+kanagarajah20091@users.noreply.github.com>
2022-11-08 18:47:30 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(550,550);
+
+    canvas = createCanvas(550,550);
+    canvas.position(560,150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+
+function modelLoaded(){
+    console.log('PoseNet Is Intializing!');
+}
+
+
+function draw()
+{
+    background('#88b8af');
+}
+
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+    }
+}
